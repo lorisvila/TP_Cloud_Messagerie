9d0fae8fc74e6674181aa08735b21bff263e782e
VILA Loris (SNCF VOYAGEURS / DIRECTION DE L'INGENIERIE DU MATERIEL / CIC - OPMV AGC) <loris.vila@sncf.fr>
2025-02-09 21:25:11 +0100

    refactor: prettying the file

--- www/listAllMessages.php
+++ www/listAllMessages.php
@@ -16,50 +16,49 @@ $result = $stmt->get_result();
     <title>TP Messagerie</title>
 </head>
 <body>
-<div id="mainContainer">
-    <div id="nav" class="pageZone" style="display: flex; flex-flow: row nowrap; justify-content: flex-start; align-items: flex-end; gap: 1em">
-        <h1>Messagerie - Tous les messages</h1>
-    </div>
-    <div id="sideBar" class="card pageZone">
-        <div class="card-header">
-            Menu
+    <div id="mainContainer">
+        <div id="nav" class="pageZone" style="display: flex; flex-flow: row nowrap; justify-content: flex-start; align-items: flex-end; gap: 1em">
+            <h1>Messagerie - Tous les messages</h1>
         </div>
-        <div class="card-body" style="display: flex; flex-flow: column nowrap; gap: 1em; justify-content: center; align-items: center; min-width: 10em">
-            <div class="btn-group-vertical" role="group" aria-label="Vertical button group">
-                <a href="/" class="btn btn-primary">Home</a>
-                <a href="/listAllMessages.php" class="btn btn-primary">Tout les messages</a>
-                <a href="http://localhost:8085/?pma_username=root&pma_password=BestPasswordEver:)&server=db" target="_blank" class="btn btn-secondary">PhpMyAdmin</a>
+        <div id="sideBar" class="card pageZone">
+            <div class="card-header">
+                Menu
+            </div>
+            <div class="card-body" style="display: flex; flex-flow: column nowrap; gap: 1em; justify-content: center; align-items: center; min-width: 10em">
+                <div class="btn-group-vertical" role="group" aria-label="Vertical button group">
+                    <a href="/" class="btn btn-primary">Home</a>
+                    <a href="/listAllMessages.php" class="btn btn-primary">Tous les messages</a>
+                    <a href="http://localhost:8085/?pma_username=root&pma_password=BestPasswordEver:)&server=db" l class="btn btn-secondary">PhpMyAdmin</a>
+                </div>
             </div>
         </div>
+
+        <div id="content" class="pageZone d-flex gap-3 flex-row">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Sender</th>
+                        <th scope="col"></th>
+                        <th scope="col">Receiver</th>
+                        <th scope="col">Message</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row['sender_id'] . "</td>";
+                    echo "<td> ➡️</td>";
+                    echo "<td>" . $row['receiver_id'] . "</td>";
+                    echo "<td>" . $row['message'] . "</td>";
+                    echo "</tr>";
+                }
+                ?>
+                </tbody>
+            </table>
+
+        </div>
     </div>
-
-    <div id="content" class="pageZone d-flex gap-3 flex-row">
-
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th scope="col">Sender</th>
-                    <th scope="col"></th>
-                    <th scope="col">Receiver</th>
-                    <th scope="col">Message</th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td>" . $row['sender_id'] . "</td>";
-                echo "<td> ➡️</td>";
-                echo "<td>" . $row['receiver_id'] . "</td>";
-                echo "<td>" . $row['message'] . "</td>";
-                echo "</tr>";
-            }
-            ?>
-            </tbody>
-        </table>
-
-    </div>
-</div>
 </body>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
